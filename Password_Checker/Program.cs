492e660db1b4f558db0449de0a724c03eed73ddb
shirley_m <shirley_m@DESKTOP-9FPOJ1O>
2024-07-05 14:22:18 +0300

    Add project files.

---
+++ Password_Checker/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Enter password again: ");
+            string pass = Console.ReadLine();
+
+            if (!password.Equals(string.Empty))
+            {
+                if (!pass.Equals(string.Empty))
+                {
+                    if (password.Length <= 30 && password.Length >= 6)
+                    {
+
+                        if (password.Equals(pass))
+                        {
+                            Console.WriteLine("Passwords match");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Passwords do not match");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please, enter a password shorter than 31 characters and/or longer than 5");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please, enter a matching password.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please, enter a password.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
